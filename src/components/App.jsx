b8f369a3bf07f012ad58dd78203964357e82419e
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-04-04 19:56:01 +0200

    statistic-component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import user from '../data/user.json';
+import data from '../data/data.json';
 
 import { Profile } from './Profile';
+import { Statistics } from './Statistics';
 
 export const App = () => {
   return (
     <div>
       <Profile user={user}></Profile>
+      <Statistics data={data}></Statistics>
     </div>
   );
 };
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ data }) => {
+  return (
+    <section className="statistics">
+      <h2 className="title">Upload stats</h2>
+
+      <ul className="stat-list">
+        {data.map(stat => (
+          <li key={stat.id} className="item">
+            <span className="label">{stat.label}</span>
+            <span className="percentage">{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ).isRequired,
+};
+
+export default Statistics;
