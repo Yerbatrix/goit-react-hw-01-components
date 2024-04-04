7eb89addacc037700e36096f5cbe875a9b9564bd
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-04-04 15:58:07 +0200

    base-profile-component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import user from '../data/user.json';
+
+import { Profile } from './Profile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Profile user={user}></Profile>
     </div>
   );
 };
